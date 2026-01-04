29d217853ee94f7f58ad56d29e90fac3dd9177de
Al Firous <53031937+AlFirous@users.noreply.github.com>
2026-01-04 22:44:47 +0700

    feat: Installed astro devtools meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,81 +2,87 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import flexokiTheme from './flexoki.json';
 
+import metaTags from "astro-meta-tags";
+
 // https://astro.build/config
 export default defineConfig({
-    compressHTML: false,
-    experimental: {
-        fonts: [
-            {
-                name: "EB Garamond",
-                cssVariable: "--font-garamond",
-                provider: fontProviders.fontsource(),
-                weights: ["400 800"],
-                subsets: ["latin"],
-                fallbacks: ["serif"],
-                optimizedFallbacks: false,
-            },
-            {
-                name: "Inter",
-                cssVariable: "--font-inter",
-                provider: fontProviders.fontsource(),
-                weights: ["100 900"],
-                fallbacks: ["sans-serif"],
-                optimizedFallbacks: false,
-            },
-            {
-                name: "Fraunces",
-                cssVariable: "--font-fraunces",
-                provider: fontProviders.fontsource(),
-                weights: ["100 900"],
-                variationSettings: "'WONK' 1",
-                fallbacks: ["serif"],
-                optimizedFallbacks: false,
-            },
-            {
-                name: "Editorial New",
-                cssVariable: "--font-editorial-new",
-                provider: "local",
-                variants: [
-                    {
-                        style: "italic",
-                        src: ["./src/assets/fonts/PPEditorialNew-UltralightItalic.otf"]
-                    },
-                    {
-                        style: "normal",
-                        src: ["./src/assets/fonts/PPEditorialNew-Ultralight.otf"]
-                    }
-                ],                
-                fallbacks: ["serif"],
-                optimizedFallbacks: false,
-            },
-            {
-                name: "Neue Montreal",
-                cssVariable: "--font-neue-montreal",
-                provider: "local",
-                variants: [
-                    {
-                        style: "normal",
-                        src: ["./src/assets/fonts/PPNeueMontreal-Book.otf"]
-                    },
-                    {
-                        style: "normal",
-                        src: ["./src/assets/fonts/PPNeueMontreal-Medium.otf"]
-                    }
-                ],                
-                fallbacks: ["sans-serif"],
-                optimizedFallbacks: false,
-            }
+  compressHTML: false,
 
-        ]
-    },
-    markdown: {
-        shikiConfig: {
-            theme: {
-                name: "Flexoki",
-                type: "dark",
-                settings: flexokiTheme.tokenColors,
-            }
-        },
-    },
+  experimental: {
+      fonts: [
+          {
+              name: "EB Garamond",
+              cssVariable: "--font-garamond",
+              provider: fontProviders.fontsource(),
+              weights: ["400 800"],
+              subsets: ["latin"],
+              fallbacks: ["serif"],
+              optimizedFallbacks: false,
+          },
+          {
+              name: "Inter",
+              cssVariable: "--font-inter",
+              provider: fontProviders.fontsource(),
+              weights: ["100 900"],
+              fallbacks: ["sans-serif"],
+              optimizedFallbacks: false,
+          },
+          {
+              name: "Fraunces",
+              cssVariable: "--font-fraunces",
+              provider: fontProviders.fontsource(),
+              weights: ["100 900"],
+              variationSettings: "'WONK' 1",
+              fallbacks: ["serif"],
+              optimizedFallbacks: false,
+          },
+          {
+              name: "Editorial New",
+              cssVariable: "--font-editorial-new",
+              provider: "local",
+              variants: [
+                  {
+                      style: "italic",
+                      src: ["./src/assets/fonts/PPEditorialNew-UltralightItalic.otf"]
+                  },
+                  {
+                      style: "normal",
+                      src: ["./src/assets/fonts/PPEditorialNew-Ultralight.otf"]
+                  }
+              ],                
+              fallbacks: ["serif"],
+              optimizedFallbacks: false,
+          },
+          {
+              name: "Neue Montreal",
+              cssVariable: "--font-neue-montreal",
+              provider: "local",
+              variants: [
+                  {
+                      style: "normal",
+                      src: ["./src/assets/fonts/PPNeueMontreal-Book.otf"]
+                  },
+                  {
+                      style: "normal",
+                      src: ["./src/assets/fonts/PPNeueMontreal-Medium.otf"]
+                  }
+              ],                
+              fallbacks: ["sans-serif"],
+              optimizedFallbacks: false,
+          }
+
+      ]
+  },
+
+  markdown: {
+      shikiConfig: {
+          theme: {
+              name: "Flexoki",
+              type: "dark",
+              settings: flexokiTheme.tokenColors,
+          }
+      },
+  },
+
+  integrations: [metaTags()],
 });
